6de0f77f819f57fd64d5b767733ce3fe0f6b16ca
root <bolodenis>
2023-11-05 07:32:27 -0800

    2-replace_in_list.py

---
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def replace_in_list(my_list, idx, element):
+   n = len(my_list)
+   if idx < n:
+       return my_list
+   elif idx >= n:
+       return my_list
+   else:
+       my_list.replace(my_list)
+       return my_list
