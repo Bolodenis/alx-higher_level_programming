44b0946078a1a7ae822d672e08a01ab1debd5100
root <bolodenis>
2023-11-08 05:46:33 -0800

    1-search_replace.py

---
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def search_replace(my_list, search, replace):
+    if search < 0 or search >= len(my_list):
+        return None
+    else:
+        new_list = my_list.copy() 
+        for i in range(len(new_list)):
+            if new_list[i] == search:
+                new_list[i] = replace
+       
+        return new_list
