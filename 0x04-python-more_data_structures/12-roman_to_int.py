727f0e77c94436597e8e0d13dca0f4483aec3a68
root <bolodenis>
2023-11-08 17:44:31 -0800

    12-roman_to_int.py

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,40 +1,20 @@
 #!/usr/bin/python3
-
-def convert_roman(ch):
-    """
-    Converts a Roman numeral character into the respective integer.
-    
-    Args:
-        ch (str): The Roman numeral character (I, V, X, L, C, D, or M).
-
-    Returns:
-        int: The corresponding integer value for the given Roman numeral character.
-    """
-    roman_to_int_map = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
-    return roman_to_int_map.get(ch, -1)
-
 def roman_to_int(roman_string):
-    """
-    Converts a string of Roman numerals to decimal.
-
-    Args:
-        roman_string (str): The input string containing Roman numerals.
-
-    Returns:
-        int: The decimal value of the given Roman numeral string.
-    """
-    if not isinstance(roman_string, str):
+    if type(roman_string) is not str or roman_string is None:
         return 0
-
-    total = 0
-    prev_value = 0
-
-    for ch in reversed(roman_string):
-        cur_value = convert_roman(ch)
-        if cur_value >= prev_value:
-            total += cur_value
-        else:
-            total -= cur_value
-        prev_value = cur_value
-
-    return total
+    roman_dict = {
+        'I': 1,
+        'V': 5,
+        'X': 10,
+        'L': 50,
+        'C': 100,
+        'D': 500,
+        'M': 1000
+    }
+    decs = [roman_dict[x] for x in roman_string]
+    output = 0
+    for i in range(len(decs)):
+        output += decs[i]
+        if decs[i - 1] < decs[i] and i != 0:
+            output -= (decs[i - 1] + decs[i - 1])
+    return output
