57fa9185aaf65466ba996e0976cf6651e12b3a2f
root <bolodenis>
2023-11-20 08:31:12 -0800

    6-raise_exception_msg.py

--- 0x05-python-exceptions/6-raise_exception_msg.py
+++ 0x05-python-exceptions/6-raise_exception_msg.py
@@ -1,6 +1,3 @@
-#!usr/bin/python3
+#!/usr/bin/python3
 def raise_exception_msg(message=""):
-    try:
-        raise Exception(message)
-    except Exception as e:
-        print("{}".format(e))
+    raise NameError(message)
