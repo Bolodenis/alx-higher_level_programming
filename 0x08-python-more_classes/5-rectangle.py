468deca29e1e96e80965cba7efb6326b4bbf98a3
root <bolodenis>
2023-11-27 10:45:58 -0800

    5-rectangle.py

--- 0x08-python-more_classes/5-rectangle.py
+++ 0x08-python-more_classes/5-rectangle.py
@@ -65,10 +65,6 @@ class Rectangle:
             string += "\n".join("#" * self.__width for j in range(self.__height))
         return string
 
-    def __repr__(self):
-        """Returns a string representation of the rectangle for reproduction"""
-        return "Rectangle({:d}, {:d})".format(self.__width, self.__height)
-
     def __del__(self):
         """Deletes the rectangle"""
         print("Bye rectangle...")
