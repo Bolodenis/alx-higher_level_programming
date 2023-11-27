62f038b2e4dc1e2bcacacc2d7f8085147d8c69d1
root <bolodenis>
2023-11-27 04:39:17 -0800

    3-rectangle.py

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -56,10 +56,9 @@ class Rectangle:
         perimeter = 0
         rectangle_perimeter = 2(self.__width + self.__height)
         return rectangle_perimeter
-
     def __str__(self):
         """returns printable string representation of the rectangle"""
         string = ""
         if self.__width != 0 and self.__height != 0:
             string += "\n".join("#" * self.__width for j in range(self.__height))
-        return string
+        return string 
