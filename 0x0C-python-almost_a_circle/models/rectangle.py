6183da350652617a5e4a02d5d26b7cc19fb3dc04
Bolodenis <bollodenis@example.com>
2023-12-11 12:11:01 -0800

    rectangle.py

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -6,109 +6,95 @@ from models.base import Base
 
 
 class Rectangle(Base):
-    """A class representing a rectangle.
-
-    Attributes:
-        width (int): The width of the rectangle.
-        height (int): The height of the rectangle.
-        x (int): The x-coordinate of the rectangle.
-        y (int): The y-coordinate of the rectangle.
-
-    Methods:
-        __init__(self, width, height, x=0, y=0, id=None): Initializes a new instance of the Rectangle class.
-        width (property): Getter and setter methods for the width property.
-        height (property): Getter and setter methods for the height property.
-        x (property): Getter and setter methods for the x property.
-        y (property): Getter and setter methods for the y property.
-        area(self): Method for finding the area of the rectangle.
-        display(self): Method for printing the rectangle using the `#` character.
-        __str__(self): String representation of the Rectangle instance.
-    """
+    '''
+        Defining the Rectangle class
+        Inherits from:
+            Base
+    '''
 
     def __init__(self, width, height, x=0, y=0, id=None):
-        """Initializes a new instance of the Rectangle class.
-
-        Args:
-            width (int): The width of the rectangle.
-            height (int): The height of the rectangle.
-            x (int, optional): The x-coordinate of the rectangle (default is 0).
-            y (int, optional): The y-coordinate of the rectangle (default is 0).
-            id (int, optional): An identifier for the object. If not provided, it is automatically generated.
-        """
-        super().__init__(id)
         self.width = width
         self.height = height
         self.x = x
         self.y = y
+        super().__init__(id)
 
     @property
     def width(self):
-        """Getter method for the width property."""
-        return self._width
-    
+        '''
+            Returning private attribute
+        '''
+        return self.__width
+
     @width.setter
     def width(self, value):
-        """Setter method for the width property."""
-        if not isinstance(value, int):
-            raise TypeError("width must be an integer")
-        if value <= 0:
-            raise ValueError("width must be > 0")
-        self._width = value
+        '''
+            Setting private attribute
+        '''
+        self.setter_validation("width", value)
+        self.__width = value
 
     @property
     def height(self):
-        """Getter method for the height property."""
-        return self._height
-    
+        '''
+            Returning private attribute
+        '''
+        return self.__height
+
     @height.setter
     def height(self, value):
-        """Setter method for the height property."""
-        if not isinstance(value, int):
-            raise TypeError("height must be an integer")
-        if value <= 0:
-            raise ValueError("height must be > 0")
-        self._height = value
+        '''
+            Setting private attribute
+        '''
+        self.setter_validation("height", value)
+        self.__height = value
 
     @property
     def x(self):
-        """Getter method for the x property."""
-        return self._x
-    
+        '''
+            Returning private attribute
+        '''
+        return self.__x
+
     @x.setter
     def x(self, value):
-        """Setter method for the x property."""
-        if value < 0:
-            raise ValueError("x must be >= 0")
-        self._x = value
+        '''
+            Setting private attribute
+        '''
+        self.setter_validation("x", value)
+        self.__x = value
 
     @property
     def y(self):
-        """Getter method for the y property."""
-        return self._y
-    
+        '''
+            Returning private attribute
+        '''
+        return self.__y
+
     @y.setter
     def y(self, value):
-        """Setter method for the y property."""
-        if value < 0:
-            raise ValueError("y must be >= 0")
-        self._y = value
+        '''
+            Setting private attribute
+        '''
+        self.setter_validation("y", value)
+        self.__y = value
 
     def area(self):
-        """Method for finding the area of the rectangle."""
-        return self.width * self.height
+        '''
+            Returns the area of the rectangle
+        '''
+        return (self.height * self.width)
 
     def display(self):
-        """Method for printing the rectangle using the `#` character."""
-        if self.width == 0 or self.height == 0:
-            print("")
-            return
-        [print("") for _ in range(self.y)]
-        for _ in range(self.height):
-            [print(" ", end="") for _ in range(self.x)]
-            [print("#", end="") for _ in range(self.width)]
+        '''
+            Prints to stdout the representation of the rectangle
+        '''
+        rectangle = ""
+        print("\n" * self.y, end="")
+        for i in range(self.height):
+            rectangle += (" " * self.x) + ("#" * self.width) + "\n"
+        print(rectangle, end="")
 
-            print("")
-    
     def update(self, *args, **kwargs):
         '''
             Updates the arguments in the class
@@ -125,19 +111,31 @@ class Rectangle(Base):
             self.y = args[4]
         except IndexError:
             pass
-        
-    def to_dictionary(self):
-         """Returns the dictionary representation of a Rectangle."""
-         return {
-        'id': self.id,
-        'width': self.width,
-        'height': self.height,
-        'x': self.x,
-        'y': self.y
-        }
 
-     def __str__(self):
-        """Return the print() and str() representation of the Rectangle."""
-        return "[Rectangle] ({}) {}/{} - {}/{}".format(self.id,
-                                                       self.x, self.y,
+    def to_dictionary(self):
+        '''
+            Returns a dictionary representation of this class
+        '''
+        return {'x': getattr(self, "x"),
+                'y': getattr(self, "y"),
+                'id': getattr(self, "id"),
+                'height': getattr(self, "height"),
+                'width': getattr(self, "width")}
+
+    @staticmethod
+    def setter_validation(attribute, value):
+        if type(value) != int:
+            raise TypeError("{} must be an integer".format(attribute))
+        if attribute == "x" or attribute == "y":
+            if value < 0:
+                raise ValueError("{} must be >= 0".format(attribute))
+        elif value <= 0:
+            raise ValueError("{} must be > 0".format(attribute))
+
+    def __str__(self):
+        '''
+            Overwritting the str method
+        '''
+        return "[Rectangle] ({}) {}/{} - {}/{}".format(self.id, self.x, self.y,
                                                        self.width, self.height)
+
